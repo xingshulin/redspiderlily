79c8ee6a4f9352841f9b1f600ef85c41dbb1c88d
Jack Wang <wangzhe.cs@gmail.com>
2015-12-21 22:37:12 +0800

    clean code

--- module/simple.py
+++ module/simple.py
@@ -10,7 +10,7 @@ import email
 
 from imapclient import IMAPClient
 
-from util import to_unicode
+from mailutil import get_subject, get_sender
 from module import settings
 
 
@@ -22,10 +22,6 @@ ssl = False
 server = IMAPClient(host, use_uid=True, ssl=ssl)
 server.login(username, password)
 
-list_folders = server.list_folders()
-for foldername in list_folders:
-    print foldername
-# studyfolder = "我的文件夹/study".decode('utf-8').encode('utf-8')
 select_info = server.select_folder(u'\u5176\u4ed6\u6587\u4ef6\u5939/test')
 print('%d messages in study' % select_info['EXISTS'])
 
@@ -33,19 +29,12 @@ messages = server.search(['NOT', 'DELETED'])
 print("%d messages that aren't deleted" % len(messages))
 
 print("Messages:")
-# response = server.fetch(messages, ['FLAGS', 'ENVELOPE'])
 response = server.fetch(messages, ['RFC822'])
-# response = server.fetch(messages, ['FLAGS', 'RFC822.SIZE'])
 for msgid, data in response.iteritems():
-    # envelop = data[b'ENVELOPE']
-    # subject = envelop.subject
-    # print('   ID %d: %s bytes, flags=%s' % (msgid,
-    # subject,
-    #                                         data[b'FLAGS'],))
     messageString = data['RFC822']
     msgStringParsed = email.message_from_string(messageString)
-    subject = email.Header.decode_header(msgStringParsed["Subject"])
-    sub = to_unicode(subject[0][0], subject[0][1])
-
+    subject = get_subject(msgStringParsed['Subject'])
+    sender = get_sender(msgStringParsed['From'])
+    date = msgStringParsed['date']
     print 'Date:%s\nFrom:%s\nSubject:%s\n%s' % \
-          (msgStringParsed['date'], msgStringParsed['From'], sub, msgStringParsed.get_payload())
+          (date, sender, subject, msgStringParsed.get_payload())
