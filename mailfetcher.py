ea006d1fd71f1486287f77ad706832bde7f0dcaf
Jack Wang <wangzhe.cs@gmail.com>
2015-12-22 16:54:25 +0800

    use envelope class rather than RFC822 to get email headers, much faster than previous process

--- mailfetcher.py
+++ mailfetcher.py
@@ -7,8 +7,10 @@
 
 from __future__ import unicode_literals
 import email
+
 from imapclient import IMAPClient
-from module.mailutil import get_subject, get_sender, get_receivers, is_reply_mail
+
+from module.mailutil import get_subject, get_sender, is_reply_mail
 from module import settings
 
 
@@ -26,9 +28,30 @@ def save_full_messages(search_ids=[]):
         if is_reply_mail(subject):
             continue
         sum_num += 1
-        print 'From:%s Subject:%s\n' % \
-              (sender, subject)
+        print 'From:%s Subject:%s\n' % (sender, subject)
     return sum_num
+
+
+def get_from_addr_from_envelope(envelope):
+    return envelope.from_[0].__str__()
+
+
+def list_message_headers(search_ids=[]):
+    sum_num = 0
+    check_num = 0
+    for msgid in search_ids:
+        check_num += 1
+        print "checking... %s" % check_num
+        response = server.fetch(msgid, ['ENVELOPE'])
+        envelope = response[msgid]['ENVELOPE']
+        subject = get_subject(envelope.subject)
+        sender = get_sender(get_from_addr_from_envelope(envelope))
+        if is_reply_mail(subject):
+            continue
+        sum_num += 1
+        print 'From:%s Subject:%s\n' % (sender, subject)
+    return sum_num
+
 
 host = settings.get('HOST')
 username = settings.get('USERNAME')
@@ -44,6 +67,7 @@ messages = server.search(['NOT', 'DELETED'])
 print("%d messages that aren't deleted" % len(messages))
 print messages
 print("Messages:")
-total_subjects = save_full_messages(messages)
+total_subjects = list_message_headers(messages)
+# total_subjects = save_full_messages(messages)
 
 print "总数为 %s" % total_subjects
