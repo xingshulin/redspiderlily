0a45fa625be781a4ca16f1148269d97f0e85b713
Jack Wang <wangzhe.cs@gmail.com>
2016-06-03 15:02:13 +0800

    remove pip

--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-pip install -r requirements.txt
-
 file="/apps/config.ini"
 if [ -f "$file" ]
 then
     cp "/apps/config.ini" "/usr/src/app/module"
 fi
 
-nohup python3 manage.py runserver 0.0.0.0:8000 &
+python3 manage.py runserver 0.0.0.0:8000
